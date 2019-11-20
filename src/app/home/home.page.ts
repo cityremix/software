bfa346c9ec971ccceaec44b5ff6c536a7578a3f2
FaPprix <46449928+FaPprix@users.noreply.github.com>
2019-11-20 15:47:26 +0100

    Home(quasi completed)
    mancano la descrizione spostare a destra infine la mappa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { R3BoundTarget } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,17 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+ciao:String;
+  constructor(private router: Router, private menu : MenuController,private prova1:Router,private prova2:Router) {
+    this.ciao='http://www.umbriaexperience.it/wp-content/uploads/Piazza-dei-bambini.jpg';
+  }
 
-  constructor(private router: Router, private menu : MenuController) {}
+  gotopage(){
+    this.prova1.navigateByUrl();
+  }
+  gotopage2(){
+    this.prova2.navigateByUrl()
+  }
     aprimenu()
     {
       this.menu.enable(true);
