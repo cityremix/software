1618b4c20cf3416f32f954fed9410dd9eead74e8
PartyPancakes16 <36924608+PartyPancakes16@users.noreply.github.com>
2019-12-14 10:35:41 +0100

    fix play button

--- src/app/archeologia/archeologia.page.ts
+++ src/app/archeologia/archeologia.page.ts
@@ -219,10 +219,51 @@ getAndSetCurrentAudioPosition() {
     });
   }, 100);
 }
+
+playRecording() {
+  this.curr_playing_file.play();
+  this.toastCtrl
+    .create({
+      message: `Start playing from ${this.fmtMSS(this.position)}`,
+      duration: 2000
+    })
+    .then(toastEl => toastEl.present());
+}
+
+pausePlayRecording() {
+  this.curr_playing_file.pause();
+  this.toastCtrl
+    .create({
+      message: `Paused at ${this.fmtMSS(this.position)}`,
+      duration: 2000
+    })
+    .then(toastEl => toastEl.present());
+}
+
 stopPlayRecording() {
   this.curr_playing_file.stop();
   this.curr_playing_file.release();
   clearInterval(this.get_position_interval);
   this.position = 0;
 }
+fmtMSS(s) {
+  return this.datePipe.transform(s * 1000, 'mm:ss');
+
+  /** The following has been replaced with Angular DatePipe */
+  // // accepts seconds as Number or String. Returns m:ss
+  // return (
+  //   (s - // take value s and subtract (will try to convert String to Number)
+  //     (s %= 60)) / // the new value of s, now holding the remainder of s divided by 60
+  //     // (will also try to convert String to Number)
+  //     60 + // and divide the resulting Number by 60
+  //   // (can never result in a fractional value = no need for rounding)
+  //   // to which we concatenate a String (converts the Number to String)
+  //   // who's reference is chosen by the conditional operator:
+  //   (9 < s // if    seconds is larger than 9
+  //     ? ':' // then  we don't need to prepend a zero
+  //     : ':0') + // else  we do need to prepend a zero
+  //   s
+  // ); // and we add Number s to the string (converting it to String as well)
+}
+
 }
